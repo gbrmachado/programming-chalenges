be2719cce744b891bdd788f5316f6e61735a6323
goma <gbrmachado@gmal.com>
2013-12-27 02:21:13 -0200

    Don't get accepted, but it is running

--- TheTrip/trip.c
+++ TheTrip/trip.c
@@ -20,28 +20,31 @@ void T_Trip_mean (T_Trip *trip) {
     double mean_aux;  /* Mean in floating */
     for (i = 0; i < trip->n; i++) 
         sum += trip->v[i];
-    
     mean_aux = ((double) sum / trip->n ) ;
-    trip->mean =  (int) (mean_aux+0.5);
+    trip->mean =  (long) (mean_aux+0.4);
 }
 
 /* Calculate the money exchanged. All the operations are made with money in cents */
 void T_Trip_exc (T_Trip *trip) {
-    long i;
-    trip->exc = 0;
-    for (i = 0; i < trip->n ; i++) 
+    long i,
+         exc_upper = 0,
+         exc_lower = 0;
+    for (i = 0; i < trip->n ; i++)            /* Upper cases */ 
         if (trip->v[i] > trip->mean) 
-            trip->exc += (trip->v[i] - trip->mean);
-
+            exc_upper += (trip->v[i] - trip->mean);
+    for (i = 0; i < trip->n ; i++)            /* Lower Cases */
+        if (trip->v[i] < trip->mean)          /* The answer will be the minor value */ 
+            exc_lower += (trip->mean - trip->v[i]);
+    trip->exc = (exc_lower < exc_upper) ? exc_lower : exc_upper;
 }
 
 int main(int argc, const char *argv[])
 {
     T_Trip trip;
     long i;
-    while ( scanf("%d", &trip.n) && trip.n != 0 ) {
+    while ( scanf("%ld", &trip.n) && trip.n != 0 ) {
         double aux;
-        trip.v = malloc( trip.n * sizeof(int) ); 
+        trip.v = malloc( trip.n * sizeof(long) ); 
         for (i = 0; i < trip.n; i++) {
             scanf("%lf", &aux);
             trip.v[i] = aux * 100 ;        /* converting to cents */
